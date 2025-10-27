78e293317144e9d9fa744bdae39073f73fd0f81d
Rafa <Rafael.EG.code@gmail.com>
2025-10-27 16:11:20 -0600

    Agrega layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-
-    </body>
-</html>
+<x-app-layout>
+    <div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio tempore atque minus sint earum omnis totam quos ea voluptatum, fugit illum magni assumenda eius iusto, fuga officiis alias unde doloribus.
+    </div>
+</x-app-layout>
