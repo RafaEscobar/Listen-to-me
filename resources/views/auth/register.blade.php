8d4e675bb16e8da1fda4456fff3499e92fa44932
Rafa <Rafael.EG.code@gmail.com>
2025-11-07 10:24:23 -0600

    AJuste

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,7 @@
     <div class="flex flex-col gap-8 bg-white rounded-xl p-4 mb-6">
         <input
             type="text"
-            placeholder="Correo electrónico"
+            placeholder="Nombre"
             class="border border-gray-300 rounded-lg px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500" />
         <input
             type="text"
