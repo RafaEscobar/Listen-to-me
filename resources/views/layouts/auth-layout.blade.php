3db623c1e4d271127fe747a5fdcd2aa56ceaaa3a
Rafa <Rafael.EG.code@gmail.com>
2025-11-07 16:18:53 -0600

    Ajuste en breakpoint de login

--- resources/views/layouts/auth-layout.blade.php
+++ resources/views/layouts/auth-layout.blade.php
@@ -13,8 +13,8 @@
 
 <body>
     <div class="h-screen p-26 bg-cyan-300">
-        <div class="flex flex-col md:flex-row gap-6 h-full bg-white rounded-xl">
-            <div class="w-full md:w-2/4 p-6">
+        <div class="flex flex-col lg:flex-row gap-6 h-full bg-white rounded-xl">
+            <div class="w-full lg:w-2/4 p-6">
                     <h1>Mensaje #1</h1>
                     <div class="flex gap-x-2 mb-4">
                         <a href="{{route('login')}}"><p>Iniciar sesión</p></a>
@@ -34,7 +34,7 @@
                         </button>
                     </div>
             </div>
-            <div class="hidden md:block md:w-2/4">
+            <div class="hidden lg:block lg:w-2/4">
                 <div class="bg-gray-200 border border-dashed border-gray-300 rounded-lg h-64 md:h-auto flex items-center justify-center">
                     <span class="text-gray-500">#Aquí va nuestra imagen#</span>
                 </div>
